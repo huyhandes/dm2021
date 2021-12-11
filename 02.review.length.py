8f792765858773e5be6f3b356b18df8500c38abb
Handes <43632522+huyhandes@users.noreply.github.com>
2021-12-11 19:04:54 +0700

    draw pdf with self calculated

--- 02.review.length.py
+++ 02.review.length.py
@@ -13,7 +13,16 @@ variance = sum(distance) / len(sample_documents_size)
 #calculate standardeviation
 
 std = math.sqrt(variance)
-
-sns_plot = sns.histplot(data=sample_documents_size,kde=True)#,x='Document size',y='counts')
+bin_size = 100
+print(sample_documents_size)
+_min = min(sample_documents_size)
+_max = max(sample_documents_size)
+pdf = [0] * int((_max - _min + bin_size)/bin_size)
+for x in sample_documents_size:
+    index = int((x-_min) / bin_size)
+    pdf[index] += 1
+print(pdf)
+x = [i*bin_size for i in range(int((_max-_min+bin_size)/bin_size))]
+sns_plot = sns.lineplot(x=x,y=pdf)#,x='Document size',y='counts')
 fig = sns_plot.get_figure()
-fig.savefig("02.review.length.png")
+fig.savefig("02.review.length1.png")
