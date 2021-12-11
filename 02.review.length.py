ea24c4643037412701d3d4e084067713637eb7e2
Handes <43632522+huyhandes@users.noreply.github.com>
2021-12-11 18:15:32 +0700

    [Labwork] update labwork 2

---
+++ 02.review.length.py
@@ -0,0 +1,19 @@
+import math
+
+from matplotlib.pyplot import xlabel
+from helper import *
+import seaborn as sns
+#calculate mean
+mean = sum(sample_documents_size) / len(sample_documents_size)
+
+#calculate variance
+distance = [(x - mean)**2 for x in sample_documents_size]
+variance = sum(distance) / len(sample_documents_size)
+
+#calculate standardeviation
+
+std = math.sqrt(variance)
+
+sns_plot = sns.histplot(data=sample_documents_size)#,x='Document size',y='counts')
+fig = sns_plot.get_figure()
+fig.savefig("output.png")
